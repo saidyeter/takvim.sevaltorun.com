d7acac0a36ca6dbef31f80d5d6548ff1dd0ac014
Said Yeter <kordiseps@gmail.com>
2023-05-06 01:09:57 +0300

    Create a mobile app #30

---
+++ mobile/app/(pages)/edit.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import { Link } from "expo-router";
+
+export default function EditPage() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>edit</Text>
+      <Link href={{ pathname: "new" }}>new</Link>
+      <Link href={{ pathname: "home" }}>home</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
