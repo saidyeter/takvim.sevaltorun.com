83ae863359ad6d1f9a49ff272b25e003814371a4
Said Yeter <kordiseps@gmail.com>
2023-05-08 23:46:01 +0300

    Implement custom view with default values #41

---
+++ mobile/components/themed/flex-view.tsx
@@ -0,0 +1,28 @@
+import { FlexStyle, View } from "react-native";
+
+export interface ViewProps extends FlexStyle {
+  children: React.ReactNode;
+  noFlex?: boolean;
+  bg?: string;
+}
+
+export function FlexView(props: ViewProps) {
+  const defaultStyle: { [key: string]: number | string } = {
+    backgroundColor: props.bg ?? "black",
+    padding: props.padding ?? 2,
+    width: props.width ?? "100%",
+  };
+  if (!props.noFlex) {
+    defaultStyle.flex = props.flex ?? 1;
+    defaultStyle.justifyContent = props.justifyContent ?? "center";
+    defaultStyle.alignItems = props.alignItems ?? "center";
+  }
+  Object.entries(props).forEach((p) => {
+    if (p[1] && (typeof p[1] === "number" || typeof p[1] === "string")) {
+      defaultStyle[p[0]] = p[1];
+    }
+  });
+  console.log("defaultStyle", defaultStyle);
+
+  return <View style={defaultStyle}>{props.children}</View>;
+}
