00a857f02c72e9c4c2d01ff3808512dafe39932b
Said Yeter <kordiseps@gmail.com>
2023-12-04 22:17:05 +0300

    Style calendar

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,63 @@
 import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
 import { dayInfo, getEvents } from "@/lib/source-api"
-import { buttonVariants } from "@/components/ui/button"
 
 export default async function IndexPage() {
   const d = await getEvents(2023, 2)
   if (!d) {
     return <>loading...</>
   }
-  console.log("data", d)
+//   console.log("data", d)
 
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <>
-        <div className="flex w-full items-end justify-between">
-          <Link
-            href={`${d.months.previousDate.year}-${d.months.previousDate.month}`}
-          >
-            <div className="flex flex-col items-center justify-center text-xl text-slate-400 hover:text-slate-200">
-              <span>{d.months.previousDate.name}</span>
-              <span>{d.months.previousDate.year}</span>
-            </div>
-          </Link>
-          <div className="flex flex-col items-center justify-center text-2xl font-bold text-white">
-            <span>{d.months.selectedDate.name}</span>
-            <span>{d.months.selectedDate.year}</span>
+    <section className="md:container p-2 flex flex-col justify-center items-center gap-6 pb-8 pt-6 md:py-10 py-4">
+      <div className="flex w-full items-end justify-between">
+        <Link
+          href={`${d.months.previousDate.year}-${d.months.previousDate.month}`}
+        >
+          <div className="flex flex-col items-center justify-center text-xl text-muted-foreground hover:text-primary">
+            <span>{d.months.previousDate.name}</span>
+            <span>{d.months.previousDate.year}</span>
           </div>
-          <Link href={`${d.months.nextDate.year}-${d.months.nextDate.month}`}>
-            <div className="flex flex-col items-center justify-center text-xl text-slate-400 hover:text-slate-200">
-              <span>{d.months.nextDate.name}</span>
-              <span>{d.months.nextDate.year}</span>
-            </div>
-          </Link>
+        </Link>
+        <div className="flex flex-col items-center justify-center text-2xl font-bold ">
+          <span>{d.months.selectedDate.name}</span>
+          <span>{d.months.selectedDate.year}</span>
         </div>
-        <>
-          <div className="mt-4 flex w-full flex-col rounded-xl bg-white/10 p-4  text-white">
-            {d.weeks.map((week, index) => {
-              return <WeekRow key={index} week={week} />
-            })}
+        <Link href={`${d.months.nextDate.year}-${d.months.nextDate.month}`}>
+          <div className="flex flex-col items-center justify-center text-xl text-muted-foreground hover:text-primary">
+            <span>{d.months.nextDate.name}</span>
+            <span>{d.months.nextDate.year}</span>
           </div>
+        </Link>
+      </div>
 
-          <div className="mt-2 flex w-full flex-col gap-1">
-            {d.events.map((value, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`flex w-full flex-col gap-1 border-l-8 bg-white/10 p-2 pl-4 text-white`}
-                  style={{ borderColor: value.dayColor }}
-                >
-                  <p className="font-light text-slate-300">
-                    {getEventDates(
-                      new Date(value.starts),
-                      new Date(value.ends)
-                    )}
-                  </p>
-                  <p className="font-semibold">{value.desc}</p>
-                </div>
-              )
-            })}
-          </div>
-        </>
-      </>
+      <div className="mt-4 flex w-full flex-col rounded-xl bg-white/10 md:p-4 p-1">
+        <div className="flex w-full space-x-1">
+          {["Pts", "Sal", "Crs", "Prs", "Cum", "Cts", "Pzr"].map((v, i) => (
+            <HeaderCell txt={v} key={i} />
+          ))}
+        </div>
+        {d.weeks.map((week, index) => {
+          return <WeekRow key={index} week={week} />
+        })}
+      </div>
+
+      <div className="mt-2 flex w-full flex-col gap-1">
+        {d.events.map((value, index) => {
+          return (
+            <div
+              key={index}
+              className={`flex w-full flex-col gap-1 border-l-8 bg-white/10 p-2 pl-4 text-white`}
+              style={{ borderColor: value.dayColor }}
+            >
+              <p className="font-light text-slate-300">
+                {getEventDates(new Date(value.starts), new Date(value.ends))}
+              </p>
+              <p className="font-semibold">{value.desc}</p>
+            </div>
+          )
+        })}
+      </div>
     </section>
   )
 }
@@ -105,25 +101,31 @@ function WeekRow(props: { week: dayInfo[] }) {
 
 function WeekCell(props: dayInfo) {
   return (
-    <>
-      <div className="flex h-16 w-full flex-col items-center justify-center rounded-md  hover:bg-white/10">
-        <div className="flex h-12 w-full flex-col items-center justify-center text-2xl">
-          <span style={{ color: props.dayColor }}>
-            {new Date(props.day ?? "")?.getDate()}
-          </span>
-        </div>
-        <div className="flex h-4 w-full flex-row items-center justify-evenly">
-          {props.info?.map((e, i) => {
-            return (
-              <div
-                key={i}
-                style={{ backgroundColor: e }}
-                className="h-2 w-2 rounded-full"
-              />
-            )
-          })}
-        </div>
+    <div className="flex md:h-16 h-10 w-full flex-col rounded-md">
+      <div className="flex md:h-12 h-8 w-full flex-col items-center justify-center md:text-2xl text-md ">
+        <span style={{ color: props.dayColor }}>
+          {new Date(props.day ?? "")?.getDate()}
+        </span>
       </div>
-    </>
+      <div className="flex md:h-4 h-2 w-full flex-row items-center justify-evenly">
+        {props.info?.map((e, i) => {
+          return (
+            <div
+              key={i}
+              style={{ backgroundColor: e }}
+              className="md:h-2 md:w-2 w-1 h-1 rounded-full"
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+function HeaderCell(props: { txt: string }) {
+  return (
+    <div className="flex bg-muted rounded-lg md:h-12 h-8 w-full flex-col items-center justify-center md:text-2xl text-muted-foreground font-bold ">
+      {props.txt}
+    </div>
   )
 }
--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -3,12 +3,11 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
-import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
-export function SiteHeader() {
+export function SiteFooter() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
+    <footer className="bg-background w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
@@ -31,6 +30,6 @@ export function SiteHeader() {
           </nav>
         </div>
       </div>
-    </header>
+    </footer>
   )
 }
